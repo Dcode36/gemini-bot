650b14d8dbc3eaa88fb92c45b59a45c333e66527
Dcode36 <kadamdigvijay36@gmail.com>
2023-12-27 18:30:39 +0530

    github project url added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
             <div className="fs-2 fw-bold">Gem<span>Bot.</span>AI</div>
             <ul className='d-flex  align-items-center  p-2 fs-5 my-2'>
                 <li><a href="https://digvijaykadam.com" target='_blank'><img src={img} alt="" /></a></li>
-                <li><a className=' mx-2 text-decoration-none text-dark' href=""><i class="bi bi-github"></i></a></li>
+                <li><a className=' mx-2 text-decoration-none text-dark' href="https://github.com/Dcode36/gemini-bot"><i class="bi bi-github"></i></a></li>
 
             </ul>
         </nav>
